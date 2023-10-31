9128e3c4d569eba089a71be646c0fbe7124eaaf0
Motun <ajirotutu4real@gmail.com>
2023-10-31 16:56:14 +0100

    Modified 14 balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,5 +7,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int bal = 0;
 
+	if (tree)
+	{
+		if (!tree->left)
+			bal += -1;
+		if (!tree->right)
+			bal += 1;
+		bal += binary_tree_balance(tree->left) - binary_tree_balance(tree->right);
+	}
+	return (bal);
 }
