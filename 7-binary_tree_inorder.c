5e04ff345df2ac8e5972e18faa8094d5f1fa16a3
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-31 10:45:23 +0000

    Task5

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - traverse a binary tree using in-order traversal
+ * @tree: pointer to the root node of the tree
+ * @func: function to call on the value of each node upon visiting
+ */
