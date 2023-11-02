7ed23019dba71a7ed6b9db319b513db38118e552
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-11-02 21:06:48 +0000

    Task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,22 +1,40 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - counts the nodes with at least 1 child in a binary tree
+ * binary_tree_is_full_check - counts the nodes with at least 1 child
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: count
+ */
+int binary_tree_is_full_check(const binary_tree_t *tree)
+{
+	int left_count, right_count;
+
+	if (tree == NULL)
+		return (1);
+
+	left_count = binary_tree_is_full(tree->left);
+	right_count = binary_tree_is_full(tree->right);
+
+	/*count = left_count ^ right_count;*/
+	if ((!tree->left && !tree->right) || (tree->left && tree->right))
+	{
+		return (left_count && right_count);
+	}
+	else
+		return (0);
+}
+
+/**
+ * binary_tree_is_full - counts the nodes with at least 1 child
  * @tree: pointer to the root node of the tree to count the number of nodes
  *
  * Return: count on success, 0 on failure
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int count, left_count, right_count;
-
 	if (tree == NULL)
 		return (0);
 
-	left_count = binary_tree_is_full(tree->left);
-	right_count = binary_tree_is_full(tree->right);
-
-	if ((!tree->left && !tree->right) || (tree->left && tree->right))
-		count = left_count && right_count;
-	return count;
+	return (binary_tree_is_full(tree));
 }
